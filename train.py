113a9194517754bed4a23837d30edbbb54df5840
Anonyvius <30316592+Anonyvius@users.noreply.github.com>
2023-01-30 17:56:45 +0100

    train

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 import glob, os
 
-#list_of_files = glob.glob('/content/drive/MyDrive/Programmieren/Python/Smarty/dataset/models/*.pt')
-#latest_file = max(list_of_files, key=os.path.getctime)
-#print(latest_file)
+list_of_files = glob.glob('/content/drive/MyDrive/Programmieren/Python/Smarty/dataset/models/*.pt')
+latest_file = max(list_of_files, key=os.path.getctime)
+print(latest_file)
 
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
@@ -12,6 +12,6 @@ trainer.setTrainConfig(
     object_names_array=["Smart"],
     batch_size=4,
     num_experiments=200#,
-    #train_from_pretrained_model=latest_file
+    train_from_pretrained_model=latest_file
     )
 trainer.trainModel()
